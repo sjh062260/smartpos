017277c63761d3707531edc71e897fbcde18b354
sjh062260 <453844704@qq.com>
2015-06-04 15:09:11 +0800

    营业员登陆成功
    第一次成功

--- smartpos/interfaceTest/sample.py
+++ smartpos/interfaceTest/sample.py
@@ -5,7 +5,7 @@ import requests
 import collections
 import json
 
-url = 'http://172.16.1.6:8080/merchant/login.json'
+url = 'http://172.16.1.201:8080/merchant/login.json'
 headers = {'x-app':'smart-pos','x-version':'10','x-deviceId':'k-201','x-sign':'08216bc91cb64412848317903070f9e3'}
 pyload = {'loginName':'01400028','password':'888888'}
 
