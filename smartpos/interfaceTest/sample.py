80d272551d3fc586702bd0edaf6d26049d259353
sjh062260 <453844704@qq.com>
2015-06-05 11:04:56 +0800

    handover/statistics
    交接班demo

--- smartpos/interfaceTest/sample.py
+++ smartpos/interfaceTest/sample.py
@@ -14,14 +14,16 @@ resp_data = resp.json()
 respData = resp_data['data']
 sessionId = respData['sessionId']
 
-handover_url = 'http://172.16.1.201:8080/handover/statistics,json'
-headersHandover= headers.update(other=['x-sessionId':sessionId])
-headoverData = {'startTime':'2015-05-10 17:34:58','endTime':'2015-05-16 17:34:58'}
-resp_handover = requests.post(handover_url,data=handoverData,headers=headers)
+handover_url = 'http://172.16.1.201:8080/handover/statistics.json'
+headersHandover= {'x-app':'smart-pos','x-version':'10','x-deviceId':'k-201','x-sign':'08216bc91cb64412848317903070f9e3','x-sessionId':sessionId}
+handoverData = {'startTime':'2015-05-10 17:34:58','endTime':'2015-05-16 17:34:58'}
+resp_handover = requests.post(handover_url,data=handoverData,headers=headersHandover)
 
 
 print(resp_data)
 print respData
 print sessionId
+print(headersHandover)
 print(resp_handover)
+print(resp_handover.text)
 
